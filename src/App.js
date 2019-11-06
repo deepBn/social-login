bd71064d92db53eca35098863221ae5ffb9f5e2f
Rajdeep Barman <deepBn9@gmail.com>
2019-11-06 11:14:21 +0530

    login with facebook working

--- src/App.js
+++ src/App.js
@@ -4,20 +4,58 @@ import './App.css';
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      authUser: null
+    };
     this.FB = window.FB;
   }
+
   componentDidMount() {
-    this.FB.getLoginStatus(function(response) {
-      console.log(response);
-      //statusChangeCallback(response);
-    });
+    this.FB.getLoginStatus(response => {
+      this.statusChangeCallback(response);
+    }, {scope: 'public_profile,email'});
   }
 
+  handleLogin = () => {
+    this.FB.login(response => {
+      this.statusChangeCallback(response);
+    }, {scope: 'public_profile,email'});
+  };
+
+  statusChangeCallback = response => {
+    console.log(response);
+    const {authResponse} = response;
+    if (response.status === 'connected') {
+      this.FB.api(
+        `/me?fields=id,name,birthday,email,picture.width(800).height(800)&access_token=${authResponse.accessToken}`, userData => {
+          const result = {
+            ...authResponse,
+            user: userData
+          };
+          this.setState({authUser: result});
+        });
+    }
+  };
+
   render() {
+    const {authUser} = this.state;
+    console.log(authUser);
+    let buttonText = 'Login with facebook';
+    let userImage = '';
+    if (authUser) {
+      buttonText = `Welcome ${authUser.user.name}`;
+      userImage = authUser.user.picture.data.url;
+    }
+
     return (
       <div className="App">
         <p>Hello World</p>
-        <button>Test</button>
+        {
+          userImage
+            ? <img src={userImage} alt="profile"/>
+            : null
+        }
+        <button onClick={this.handleLogin}>{buttonText}</button>
       </div>
     );
   }
